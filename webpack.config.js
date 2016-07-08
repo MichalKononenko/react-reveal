ac7a5f70931aea5aad35547c7baaead013788cd9
Michal Kononenko <michalkononenko@gmail.com>
2016-07-08 00:36:51 -0400

    Added more dev environment stuff

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var build_directory = path.join(__dirname, 'build');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: build_directory, filename: 'bundle.min.js',
+        publicPath: "/build"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /![index]\.html$/,
+                loader: "url-loader?limit=10000"
+            },
+            {
+                test: /index\.html$/, loader: "file-loader?name=index.html"
+            },
+            {
+                test: /(\.jpe?g$)|(\.svg)|(\.png)/, loader: "url-loader"
+            }
+        ]
+    }
+};
